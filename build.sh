a4ed89b8dbd4886bb586102f3dd5cba14b0e705d
invictusmiguel <todasmisiamiguel@gmail.com>
2025-04-20 09:54:43 -0500

    Fix total: forzar entorno con ensurepip y sin caché

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip setuptools wheel --no-cache-dir
+python3 -m pip install -r requirements.txt --no-cache-dir
